78d664ea7d27318095c046ce10fe52960aee96c0
Zenoviia <zenoviatkacenko@gmail.com>
2025-01-04 01:21:55 +0200

    create constant for options

--- utils.js
+++ utils.js
@@ -1,3 +1,9 @@
-const asyncify = async () => {
-    
-  };
+const asyncify = async (task, options = {}) => {
+  const {
+    minIterations = 1,
+    maxIterations = Infinity,
+    minDuration = 0,
+    maxDuration = Infinity,
+    timeout = 0,
+  } = options;
+};
