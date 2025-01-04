6806016d035525575a9aff4b2fabb82f5bdea977
Zenoviia <zenoviatkacenko@gmail.com>
2025-01-04 02:52:55 +0200

    add import

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import asyncify from "./utils.js";
+
 const isPrime = (n) => {
   if (n <= 1) return false;
   for (let i = 2; i <= Math.sqrt(n); i++) {
@@ -14,4 +16,14 @@ const primeTask = (() => {
       console.log(`Простое число найдено: ${number}`);
     }
   };
+})();
+
+(async () => {
+  await asyncify(primeTask, {
+    minIterations: 5, // min num of iterations
+    maxIterations: 10, // max num of iterations
+    minDuration: 1000, // min interval between iterations (1 second)
+    maxDuration: 60000, // max interval between iterations (60 seconds)
+    timeout: 2000, // pause between bloks of iterations (2 seconds)
+  });
 })();
