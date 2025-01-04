ee869222d1833a46f68d80b678a510f282941d8e
Zenoviia <zenoviatkacenko@gmail.com>
2025-01-04 02:33:01 +0200

    Update function logIterationStats and export

--- utils.js
+++ utils.js
@@ -11,43 +11,44 @@ const asyncify = async (task, options = {}) => {
   let totalIterations = 0;
 
   const pause = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-};
 
-const logIterationStats = (iterations, duration) => {
-    console.log(`Performed ${iterations} iterations in ${duration} ms.`);
+  const logIterationStats = (iterations, duration) => {
+    console.log(`Выполнено ${iterations} итераций за ${duration} мс.`);
   };
-  
+
   try {
     while (Date.now() - startTime < maxDuration) {
       const blockStartTime = Date.now();
       let blockIterations = 0;
-  
+
       while (blockIterations < maxIterations) {
         task();
         blockIterations++;
         totalIterations++;
-  
+
         const elapsedTimeBlock = Date.now() - blockStartTime;
         const hasCompletedMinIterations = totalIterations >= minIterations;
         const hasCompletedMinDuration = elapsedTimeBlock >= minDuration;
-  
+
         if (hasCompletedMinIterations && hasCompletedMinDuration) {
           break;
         }
       }
-  
+
       const blockDuration = Date.now() - blockStartTime;
       const isMaxIterationsReached = blockIterations >= maxIterations;
       const isMaxDurationReached = blockDuration >= maxDuration;
-  
+
       if (isMaxIterationsReached || isMaxDurationReached) {
         logIterationStats(blockIterations, blockDuration);
         await pause(timeout);
       }
     }
-  
-    console.log(`The task is complete. Total iterations: ${totalIterations}`);
+
+    console.log(`Задача завершена. Всего итераций: ${totalIterations}`);
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Произошла ошибка:", error);
   }
-  
+};
+
+export default asyncify;
